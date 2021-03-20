0ad5f74eed2c8387ca3c9e7ac6577193a4f94e25
darshansatra1 <darshansatra1@gmail.com>
2021-03-20 11:25:20 +0530

    added the game state logic

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -8,11 +8,27 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    
+    int activePlayer = 0;
+    int[] gameState= {-1,-1,-1,-1,-1,-1,-1,-1,- 1};
+
+
     public void dropIn(View view){
         ImageView counter = (ImageView) view;
-        counter.setTranslationY(-1000f);
-        counter.setImageResource(R.drawable.yellow_marker);
-        counter.animate().translationYBy(1000f).setDuration(300);
+        int tappedCounter = Integer.parseInt(counter.getTag().toString());
+        if(gameState[tappedCounter]==-1) {
+            gameState[tappedCounter]=activePlayer;
+            counter.setTranslationY(-1000f);
+            if (activePlayer == 0) {
+                counter.setImageResource(R.drawable.zero_marker);
+                activePlayer = 1;
+            } else {
+                counter.setImageResource(R.drawable.cross_marker);
+                activePlayer = 0;
+            }
+            counter.animate().translationYBy(1000f).rotation(360).setDuration(400);
+        }
+
     }
 
     @Override
